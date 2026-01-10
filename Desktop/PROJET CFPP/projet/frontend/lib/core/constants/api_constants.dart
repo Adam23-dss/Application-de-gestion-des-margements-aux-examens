11ce1940a78da398072bf5e71aa30dd502785566
coulibayaboubacar <aboubacar6676@gmail.com>
2026-01-10 21:01:39 +0000

    Changement de l'url pour la communication avec le backEnd

--- Desktop/PROJET CFPP/projet/frontend/lib/core/constants/api_constants.dart
+++ Desktop/PROJET CFPP/projet/frontend/lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "http://localhost:5000";
+  static const String baseUrl = "http://localhost:3000";
 
   static const int connectTimeoutMs = 15000;
   static const int receiveTimeoutMs = 15000;
