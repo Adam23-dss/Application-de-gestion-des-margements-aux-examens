d50cb54efc276ad77efe6cf14da6aab8c8aad0a9
Adam23-DSS <adoummahamatnourhamid326@gmail.com>
2026-01-11 02:01:03 +0000

    Modification des packages

---
+++ frontend/lib/core/constants/app_constants.dart
@@ -0,0 +1,3 @@
+// lib/core/constants/api_constants.dart
+const String apiBaseUrl = 'https://application-de-gestion-des-margements.onrender.com';
+const String wsUrl = 'wss://application-de-gestion-des-margements.onrender.com';
